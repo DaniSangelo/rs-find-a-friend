e09376bc5f5692a668f2ca080a1ca2ff6afd5e56
Daniel Sângelo <danielsangelo@hotmail.com>
2024-10-12 10:03:00 -0300

    Create company unit test  #ISS-0016 [feature]

--- src/Domain/Entities/Company.ts
+++ src/Domain/Entities/Company.ts
@@ -1,15 +1,16 @@
-import { PasswordType } from '../Enums/PasswordTypeEnum'
+import { PasswordTypeEnum } from '../Enums/PasswordTypeEnum'
 import PasswordFactory from '../Factories/PasswordFactory'
 import Email from '../ValueObjects/Email'
 import Name from '../ValueObjects/Name'
 import Password from '../ValueObjects/Password'
+import PhoneNumber from '../ValueObjects/PhoneNumber'
 
 export default class Company {
   private _ownerName: Name
   private _email: Email
   private _CEP: string
   private _address: string
-  private _whatsAppNumber: string
+  private _whatsAppNumber: PhoneNumber
   private _password: Password
 
   constructor(
@@ -19,13 +20,34 @@ export default class Company {
     address: string,
     whatsAppNumber: string,
     password: string,
+    readonly passwordType: PasswordTypeEnum = PasswordTypeEnum.SHA1,
   ) {
     this._ownerName = new Name(ownerName)
     this._email = new Email(email)
     this._CEP = CEP
     this._address = address
-    this._whatsAppNumber = whatsAppNumber
-    this._password = PasswordFactory.create(password, PasswordType.SHA1)
+    this._whatsAppNumber = new PhoneNumber(whatsAppNumber)
+    this._password = PasswordFactory.create(password, passwordType)
+  }
+
+  static create(
+    ownerName: string,
+    email: string,
+    CEP: string,
+    address: string,
+    whatsAppNumber: string,
+    password: string,
+    passwordType: PasswordTypeEnum = PasswordTypeEnum.SHA1,
+  ) {
+    return new Company(
+      ownerName,
+      email,
+      CEP,
+      address,
+      whatsAppNumber,
+      password,
+      passwordType,
+    )
   }
 
   getEmail() {
@@ -34,5 +56,20 @@ export default class Company {
 
   getName() {
     return this._ownerName.get()
+  }
+
+  setEmail(email: string) {
+    this._email = new Email(email)
+  }
+
+  setPassword(
+    password: string,
+    passwordType: PasswordTypeEnum = PasswordTypeEnum.SHA1,
+  ) {
+    this._password = PasswordFactory.create(password, passwordType)
+  }
+
+  setPhoneNumber(value: string) {
+    this._whatsAppNumber = new PhoneNumber(value)
   }
 }
