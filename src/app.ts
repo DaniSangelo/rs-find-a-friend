417cea41cce48a320fa7b2bb0685f9af6ccd36b5
Daniel Sângelo <danielsangelo@hotmail.com>
2024-11-23 12:12:54 -0300

    Create main file #ISS-0026 [enhancement]

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,30 @@
-import fastify from 'fastify'
+import { FastifyInstance } from 'fastify'
 import { companyRoutes } from './Adapters/HTTP/Controllers/Company/Routes'
 import { ZodError } from 'zod'
 import 'dotenv/config'
 
-export const app = fastify()
-app.register(companyRoutes, {
-  prefix: '/company',
-})
-
-app.setErrorHandler((error, _, reply) => {
-  if (error instanceof ZodError) {
-    return reply.status(400).send({
-      message: 'Validation error',
-      issues: error.format(),
-    })
-  }
-  if (process.env.NODE_ENV !== 'production') {
-    console.error(error)
-  } else {
-    // todo: implement log
-  }
-  return reply.status(500).send({ message: 'Internal server error' })
-})
+export function createApp(
+  app: FastifyInstance,
+  dependencies: {
+    companyRegistrationController: ReturnType<typeof companyRoutes>
+  },
+) {
+  app.register(companyRoutes(dependencies.companyRegistrationController), {
+    prefix: 'company',
+  })
+  app.setErrorHandler((error, _, reply) => {
+    if (error instanceof ZodError) {
+      return reply.status(400).send({
+        message: 'Validation error',
+        issues: error.format(),
+      })
+    }
+    if (process.env.NODE_ENV !== 'production') {
+      console.error(error)
+    } else {
+      // todo: implement log
+    }
+    return reply.status(500).send({ message: 'Internal server error' })
+  })
+  return app
+}
